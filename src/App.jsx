78dc514ff71c3b4b08eeb224753c636747099204
Peniel Bekele <142409326+Penie1@users.noreply.github.com>
2024-08-07 15:37:10 +0300

    Lift state to App component for conditional CSS styling

--- src/App.jsx
+++ src/App.jsx
@@ -29,25 +29,47 @@ function App() {
     endYear: "",
   });
   const [professionalSummary, setProfessionalSummary] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  function handleOnClick() {
+    setIsSubmitted(!isSubmitted);
+  }
 
   return (
-    <main className="text-sm text-bold text-slate-700 font-mono grid sm:grid-cols-2 gap-6  min-h-dvh p-5  bg-gray-200 ">
-      <Form
-        generalInfo={generalInfo}
-        setGeneralInfo={setGeneralInfo}
-        workExp={workExp}
-        setWorkExp={setWorkExp}
-        educationalExp={educationalExp}
-        setEducationalExp={setEducationalExp}
-        professionalSummary={professionalSummary}
-        setProfessionalSummary={setProfessionalSummary}
-      />
-      <DisplayFormComponent
-        generalInfo={generalInfo}
-        workExp={workExp}
-        educationalExp={educationalExp}
-        professionalSummary={professionalSummary}
-      />
+    <main>
+      <div
+        className={`text-sm text-bold text-slate-700 font-mono grid gap-6 min-h-dvh p-5 bg-gray-200 ${
+          isSubmitted ? "sm:grid-cols-1" : "sm:grid-cols-2"
+        }`}
+      >
+        <Form
+          generalInfo={generalInfo}
+          setGeneralInfo={setGeneralInfo}
+          workExp={workExp}
+          setWorkExp={setWorkExp}
+          educationalExp={educationalExp}
+          setEducationalExp={setEducationalExp}
+          professionalSummary={professionalSummary}
+          setProfessionalSummary={setProfessionalSummary}
+          isSubmitted={isSubmitted}
+          setIsSubmitted={setIsSubmitted}
+        />
+        <DisplayFormComponent
+          generalInfo={generalInfo}
+          workExp={workExp}
+          educationalExp={educationalExp}
+          professionalSummary={professionalSummary}
+        />
+      </div>
+
+      {isSubmitted && (
+        <button
+          className="border-1  border-amber-600 max-w-32 px-5 py-2 rounded-md  -col-start-2 bg-yellow-600 text-white hover:text-slate-700"
+          onClick={handleOnClick}
+        >
+          Edit
+        </button>
+      )}
     </main>
   );
 }
--- src/Form.jsx
+++ src/Form.jsx
@@ -3,7 +3,6 @@ import EducationalExp from "./components/EducationalExp";
 import GeneralInfo from "./components/GeneralInfo";
 import WorkExp from "./components/WorkExp";
 import ProfessionalSummary from "./components/ProfessionalSummary";
-import { useState } from "react";
 
 export default function Form({
   generalInfo,
@@ -14,27 +13,14 @@ export default function Form({
   setEducationalExp,
   professionalSummary,
   setProfessionalSummary,
+  isSubmitted,
+  setIsSubmitted,
 }) {
-  const [isSubmitted, setIsSubmitted] = useState(false);
-
   function handleSubmit(e) {
     e.preventDefault();
     setIsSubmitted(!isSubmitted);
   }
-  function handleOnClick() {
-    setIsSubmitted(!isSubmitted);
-  }
-  if (isSubmitted) {
-    return (
-      <button
-        className=" absolute bottom-0 right-0 border-1  border-amber-600 max-w-32 px-5 py-2 rounded-md  -col-start-2 bg-yellow-600 text-white hover:text-slate-700"
-        onClick={handleOnClick}
-      >
-        Edit
-      </button>
-    );
-  }
-
+  if (isSubmitted) return;
   return (
     <form onSubmit={handleSubmit} className="grid gap-14 " autoComplete="on">
       <GeneralInfo generalInfo={generalInfo} setGeneralInfo={setGeneralInfo} />
@@ -66,4 +52,6 @@ Form.propTypes = {
   setEducationalExp: PropTypes.func.isRequired,
   professionalSummary: PropTypes.string.isRequired,
   setProfessionalSummary: PropTypes.func.isRequired,
+  isSubmitted: PropTypes.bool.isRequired,
+  setIsSubmitted: PropTypes.func.isRequired,
 };
